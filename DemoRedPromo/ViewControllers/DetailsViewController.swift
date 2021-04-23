5c3754ae0ea0193dc140a505ff12e34276cad1f3
Albert <bool.flag@gmail.com>
2021-04-23 21:11:00 +0500

    Initial Commit

---
+++ DemoRedPromo/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  DemoRedPromo
+//
+//  Created by Альберт Садыков on 23.04.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
